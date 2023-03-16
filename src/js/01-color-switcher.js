0df1343da04f0ec44d7f7a2ce169efe81744bd69
Mykkolla <tarabukin.n.10@gmail.com>
2023-03-16 21:01:45 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
 
+
+const btnStart = document.querySelector('[data-start]')
+const btnStop = document.querySelector('[data-stop]')
+const body = document.querySelector("body");
+
+const handleClickStart = () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();   
+      }, 1000);
+      console.log(`Start`);
+      
+      btnStart.setAttribute('disabled', true); 
+      if (btnStart.hasAttribute('disabled')) {
+        btnStop.removeAttribute('disabled');
+    }
+  };
+
+  const handleClickStop = () => {
+    clearInterval(timerId);
+    console.log(`Stop`);
+    
+    btnStop.setAttribute('disabled', true);
+    if (btnStop.hasAttribute('disabled')) {
+        btnStart.removeAttribute('disabled');
+    }
+  };
+
+
+
+btnStart.addEventListener("click", handleClickStart);
+
+btnStop.addEventListener("click", handleClickStop);
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
